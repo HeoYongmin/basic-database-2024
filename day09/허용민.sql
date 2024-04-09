3a8d62407ab2e87a7035f7cbd440b0fa8a426b3a
HeoYongmin <snrn2634@naver.com>
2024-04-09 12:15:54 +0900

    오전 종료

---
+++ day09/허용민.sql
@@ -0,0 +1,34 @@
+﻿-- 1. 회원 테이블에서 이메일, 모바일, 이름, 주소 순으로 나오도록 검색하시오.
+SELECT Email
+     , Mobile
+	 , Names
+	 , Addr
+  FROM membertbl
+ ORDER BY Addr DESC;
+
+-- 2. 함수를 사용하여 아래와 같은 결과가 나오도록 쿼리를 작성하시오.(전채 행의 개수는 59개입니다)
+SELECT Names AS '도서명'
+     , Author AS '저자'
+	 , ISBN
+	 , Price AS '정가'
+  FROM bookstbl
+ ORDER BY Price DESC;
+
+-- 3.  다음과 같은 결과가 나오도록 SQL 문을 작성하시오.(책을 한번도 빌린적이 없는 회원을 뜻합니다)
+SELECT DISTINCT m.[Names] AS '회원명',
+                m.Levels AS '회원등급',
+                m.Addr AS '회원주소',
+                r.RentalDate AS '대여일'
+  FROM membertbl AS m LEFT JOIN rentaltbl AS r
+    ON m.memberIdx = r.memberIdx
+ WHERE r.memberIdx IS NULL OR r.RentalDate IS NULL
+ ORDER BY Levels ASC;
+
+-- 4. 다음과 같은 결과가 나오도록 SQL 문을 작성하시오.
+SELECT d.Names AS '책 장르',
+       SUM(b.Price) AS '총합계금액'
+  FROM divtbl AS d LEFT JOIN bookstbl AS b 
+    ON d.Division = b.Division
+ WHERE b.Price IS NOT NULL
+GROUP BY d.Names;
+
